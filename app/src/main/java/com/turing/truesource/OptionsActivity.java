0b75a1c155f9ed56adb7cc9d76424a8347d3c64e
chalie <chalie@localhost.localdomain>
2022-03-12 16:45:33 +0300

    first app template commit

---
+++ app/src/main/java/com/turing/truesource/OptionsActivity.java
@@ -0,0 +1,14 @@
+package com.turing.truesource;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
