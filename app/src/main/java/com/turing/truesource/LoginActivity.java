25009f251849a4eba497fe1e56f1d9002a13bf71
chalie <chalie@localhost.localdomain>
2022-03-12 17:44:21 +0300

    add login ui

--- app/src/main/java/com/turing/truesource/LoginActivity.java
+++ app/src/main/java/com/turing/truesource/LoginActivity.java
@@ -2,13 +2,31 @@ package com.turing.truesource;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signup = (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == signup){
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
